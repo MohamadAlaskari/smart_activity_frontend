dcfd81916e24246b2fb3d3eb5ef01c5aae80e25d
MohamadAlaskari <m_alskari96@hotmail.com>
2025-06-21 18:32:09 +0200

    weather eigenschaften

--- lib/domain/model/weather.dart
+++ lib/domain/model/weather.dart
@@ -11,35 +11,9 @@ class Weather with _$Weather {
     required String date,
     required double tempmax,
     required double tempmin,
-    required double temp,
-    required double feelslikemax,
-    required double feelslikemin,
-    required double feelslike,
-    required double dew,
-    required double humidity,
-    required double precip,
-    required double precipprob,
-    required double precipcover,
-    List<String?>? preciptype,
-    required double snow,
-    required double snowdepth,
-    required double windgust,
-    required double windspeed,
-    required double winddir,
-    required double pressure,
-    required double cloudcover,
-    required double visibility,
-    required double solarradiation,
-    required double solarenergy,
-    required double uvindex,
-    required double severerisk,
-    required String sunrise,
-    required String sunset,
-    required double moonphase,
-    required String conditions,
-    required String description,
     required String icon,
   }) = _Weather;
 
-  factory Weather.fromJson(Map<String, dynamic> json) => _$WeatherFromJson(json);
+  factory Weather.fromJson(Map<String, dynamic> json) =>
+      _$WeatherFromJson(json);
 }
